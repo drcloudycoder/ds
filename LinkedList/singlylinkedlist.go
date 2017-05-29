2df9f6e0b2f0aca1bb631f70d062e6827f6646eb
Dr Cloudy Coder <drcloudycoder@gmail.com>
2017-05-29 13:33:56 -0700

    Add function signatures to blueprint

--- LinkedList/singlylinkedlist.go
+++ LinkedList/singlylinkedlist.go
@@ -5,47 +5,83 @@ import (
 )
 
 // Represents structure of single item of list
-
+type Node struct {
+  data int
+  next *Node
+}
 
 // Represents structure of the List
-
+type List struct {
+  head *Node
+  size int
+}
 
 /*
   Create an empty list
 */
+func New() *List {
+  return &List{size:0}
+}
 
 /*
   Get the list's current length
 */
+func (list *List) Size() int {
+  return list.size
+}
 
 /*
   Get the head of the list
-/*
+*/
+func (list *List) Head() *Node {
+  return list.head
+}
 
 /*
   Append an element to the end of list
 */
+func (list *List) AppendToEnd(data int) {
+
+}
 
 /*
   Prepend an element to the beginning of the list
 */
+func (list *List) PrependToBeginning(data int) {
+
+}
 
 /*
   Insert an element at specific position in the list
 */
+func (list *List) InsertAt(data int, position int) {
+
+}
 
 /*
   Delete an element from the end of the list
 */
+func (list *List) DeleteFromEnd() {
+
+}
 
 /*
   Delete an element from beginning of the list
 */
+func (list *List) DeleteFromBeginning() {
+
+}
 
 /*
   Delete a specific element from the list
 */
+func (list *List) DeleteElement(data int) {
+
+}
 
 /*
   Find an element
 */
+func (list *List) FindPositionofElement(data int) {
+
+}
