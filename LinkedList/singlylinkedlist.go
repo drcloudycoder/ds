bc1da6b41dbcde55a9c6e6619b3a119e1b6c6130
Dr Cloudy Coder <drcloudycoder@gmail.com>
2017-05-29 13:21:42 -0700

    Add blueprint for Singly Linked List

---
+++ LinkedList/singlylinkedlist.go
@@ -0,0 +1,51 @@
+package linkedlist
+
+import (
+  "fmt"
+)
+
+// Represents structure of single item of list
+
+
+// Represents structure of the List
+
+
+/*
+  Create an empty list
+*/
+
+/*
+  Get the list's current length
+*/
+
+/*
+  Get the head of the list
+/*
+
+/*
+  Append an element to the end of list
+*/
+
+/*
+  Prepend an element to the beginning of the list
+*/
+
+/*
+  Insert an element at specific position in the list
+*/
+
+/*
+  Delete an element from the end of the list
+*/
+
+/*
+  Delete an element from beginning of the list
+*/
+
+/*
+  Delete a specific element from the list
+*/
+
+/*
+  Find an element
+*/
