d90db66cf0534f995648d20cd3803492cde56050
Dr Cloudy Coder <drcloudycoder@gmail.com>
2017-05-29 20:22:21 -0700

    Add a test case for InsertAfter

--- LinkedList/singlylinkedlist_test.go
+++ LinkedList/singlylinkedlist_test.go
@@ -33,3 +33,19 @@ func TestPrependToBeginning(t *testing.T) {
     current = current.next
   }
 }
+
+func TestInsertAfter(t *testing.T) {
+  list := New()
+  fmt.Println("Size - ", list.Size())
+  list.AppendToEnd(2)
+  list.AppendToEnd(3)
+  list.AppendToEnd(5)
+  list.PrependToBeginning(6)
+  list.InsertAfter(10,3)
+  fmt.Println("Size - ", list.Size())
+  current := list.Head()
+  for current != nil {
+    fmt.Println("Element: ", current.data)
+    current = current.next
+  }
+}
